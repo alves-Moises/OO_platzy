7c8362c9edf592ceffc53cc63c6f5b1ba40077bf
alves-Moises <moisesinho_ra@hotmail.com>
2022-08-21 10:23:20 -0300

    ubber main java update

--- Java/uber/Main.java
+++ Java/uber/Main.java
@@ -2,15 +2,14 @@ class Main {
     public static void main(String[] args){
 
         System.out.println("holA Mundo");
-        Car car = new Car();
-        car.license = "AMQ1213;";
-        car.driver = "Andres Herrera";
-        car.passegenger = 4;
+        Car car = new Car("AMQ1213", new Account("Andres Herrera", "AMD123"));
+        // car.license = "AMQ1213;";
+        // car.driver = "Andres Herrera";
+        // car.passegenger = 4;
         car.printDataCar();
         
-        Car car2 = new Car();
+        Car car2 = new Car("QWE456", new Account("Adrea", "ANDA789"));
         car2.license = "QWE4567";
-        car2.driver = "Andrea Herera";
         car2.passegenger = 3;
         System.out.println("Car license: " + car2.license);
         car2.printDataCar();
