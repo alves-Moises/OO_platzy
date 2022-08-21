acb678715f885d221751a5420b3229eadc1d5479
alves-Moises <moisesinho_ra@hotmail.com>
2022-08-21 10:23:11 -0300

    ubber car java update

--- Java/uber/Car.java
+++ Java/uber/Car.java
@@ -1,10 +1,17 @@
 public class Car {
     Integer id;
     String license;
-    String driver;
+    Account driver;
     Integer passegenger;
+
+    public Car(String license, Account driver){
+        this.license = license;
+        this.driver = driver;
+
+
+    }
     
     void printDataCar(){
-        System.out.println("License: " + license + " Driveer: " + driver);
+        System.out.println("License: " + license + " Driveer: " + driver.name);
     }
 }
